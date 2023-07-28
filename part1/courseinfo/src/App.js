4d02c7ff29614f870dcb54d5e7fa0bed18394a02
buchii1 <oko22007@byui.edu>
2023-07-28 23:34:36 +0100

    step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,12 +1,13 @@
 const Header = (props) => {
 	return (
 		<>
-			<h1>{props.course}</h1>
+			<h1>{props.name}</h1>
 		</>
 	);
 };
 
 const Content = (props) => {
+	console.log(props);
 	const parts = props.parts;
 
 	return (
@@ -35,27 +36,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-	const course = "Half Stack application development";
-	const parts = [
-		{
-			name: "Fundamentals of React",
-			exercises: 10,
-		},
-		{
-			name: "Using props to pass data",
-			exercises: 7,
-		},
-		{
-			name: "State of a component",
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course} />
-			<Content parts={parts} />
-			<Total parts={parts} />
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
