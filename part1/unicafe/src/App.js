0b33f0ca5ff7687ed41e462ed813b573fbe5213c
buchii1 <oko22007@byui.edu>
2023-08-04 03:09:28 +0100

    step3 & step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,21 +4,51 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [scores, setScores] = useState([]);
 
-	const goodFeedback = () => setGood(good + 1);
-	const neutralFeedback = () => setNeutral(neutral + 1);
-	const badFeedback = () => setBad(bad + 1);
+	const handleGoodFeedback = () => {
+		setGood(good + 1);
+		setScores(scores.concat(1));
+	};
+
+	const handleNeutralFeedback = () => {
+		setNeutral(neutral + 1);
+		setScores(scores.concat(0));
+	};
+
+	const handleBadFeedback = () => {
+		setBad(bad + 1);
+		setScores(scores.concat(-1));
+	};
+
+	let totScore = 0;
+	let totPositiveNum = 0;
+
+	for (let i = 0; i < scores.length; i++) {
+		totScore += scores[i];
+
+		if (scores[i] > 0) {
+			totPositiveNum += scores[i];
+		}
+	}
+
+	const total = good + neutral + bad;
+	const average = totScore / scores.length;
+	const positive = (totPositiveNum / scores.length) * 100;
 
 	return (
 		<div>
 			<h1>give feedback</h1>
-			<Button handleClick={goodFeedback} text="good" />
-			<Button handleClick={neutralFeedback} text="neutral" />
-			<Button handleClick={badFeedback} text="bad" />
+			<Button handleClick={handleGoodFeedback} text="good" />
+			<Button handleClick={handleNeutralFeedback} text="neutral" />
+			<Button handleClick={handleBadFeedback} text="bad" />
 			<h1>statistics</h1>
 			<Statistics stats={good} name="good" />
 			<Statistics stats={neutral} name="neutral" />
 			<Statistics stats={bad} name="bad" />
+			<Statistics stats={total} name="all" />
+			<Statistics stats={average} name="average" />
+			<Statistics stats={`${positive} %`} name="positive" />
 		</div>
 	);
 };
