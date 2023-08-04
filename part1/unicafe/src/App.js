7e06814b444596af67b67ef8c6d4f67b44543ece
buchii1 <oko22007@byui.edu>
2023-08-04 04:16:41 +0100

    step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -21,15 +21,6 @@ const App = () => {
 		setScores(scores.concat(-1));
 	};
 
-	let totScore = 0;
-	for (let i = 0; i < scores.length; i++) {
-		totScore += scores[i];
-	}
-
-	const total = good + neutral + bad;
-	const average = totScore / scores.length;
-	const positive = (good / total) * 100;
-
 	return (
 		<div>
 			<h1>give feedback</h1>
@@ -37,14 +28,7 @@ const App = () => {
 			<Button handleClick={handleNeutralFeedback} text="neutral" />
 			<Button handleClick={handleBadFeedback} text="bad" />
 			<h1>statistics</h1>
-			<Statistics
-				good={good}
-				neutral={neutral}
-				bad={bad}
-				total={total}
-				avg={average}
-				positive={positive}
-			/>
+			<Statistics good={good} neutral={neutral} bad={bad} scores={scores} />
 		</div>
 	);
 };
@@ -54,7 +38,16 @@ const Button = ({ handleClick, text }) => (
 );
 
 const Statistics = (props) => {
-	const { good, neutral, bad, total, avg, positive } = props;
+	const { good, neutral, bad, scores } = props;
+
+	let totScore = 0;
+	for (let i = 0; i < scores.length; i++) {
+		totScore += scores[i];
+	}
+
+	const total = good + neutral + bad;
+	const average = totScore / total;
+	const positive = (good / total) * 100;
 
 	return (
 		<div>
@@ -62,7 +55,7 @@ const Statistics = (props) => {
 			<StatisticsLine value={neutral} text="neutral" />
 			<StatisticsLine value={bad} text="bad" />
 			<StatisticsLine value={total} text="all" />
-			<StatisticsLine value={avg} text="average" />
+			<StatisticsLine value={average} text="average" />
 			<StatisticsLine value={`${positive} %`} text="positive" />
 		</div>
 	);
